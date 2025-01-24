e2571722e638609e5eebfc3abbbce43fdb55e4f8
Osuolale <mohd@yahoo.com>
2025-01-24 20:16:09 +0100

    Fixing Bugs 3

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -58,7 +58,7 @@ export const FloatingNav = ({
           className
         )}
       >
-        {navItems.map((navItem: any, idx: number) => (
+        {navItems.map((navItem, idx) => (
           <Link
             key={`link=${idx}`}
             href={navItem.link}
