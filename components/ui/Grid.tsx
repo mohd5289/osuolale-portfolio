b10cfce64d6694c74bbca74f0356dea3dcc48ae1
Osuolale <mohd@yahoo.com>
2025-01-25 00:58:42 +0100

    Fixing Bugs 6

--- components/ui/Grid.tsx
+++ components/ui/Grid.tsx
@@ -3,6 +3,12 @@ import { BentoGrid, BentoGridItem } from "./BentoGrid";
 import { gridItems } from "@/data";
 
 const Grid = () => {
+  // useEffect(() => {
+  //   // This code will only run on the client
+  //   const element = document.getElementById("about");
+  //   // Perform actions with the element
+  //   console.log(element);
+  // }, []);
   return (
     <section id="about">
       <BentoGrid>
