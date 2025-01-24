b18fa8e453697b474985d8d9e230feae14d7cf1d
Osuolale <mohd@yahoo.com>
2025-01-25 00:12:12 +0100

    Fixing Bugs 6

--- app/page.tsx
+++ app/page.tsx
@@ -14,30 +14,30 @@ export default function Home() {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true);
+    // This code will only run on the client
+    const element = document.getElementById("some-element");
+    // Perform actions with the element
   }, []);
   return (
-    <div>
-      {isMounted && (
-        <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
-          (
-          <div className="max-w-7xl w-full flex flex-col">
-            <FloatingNav navItems={navItems} />
-            <Hero />
-            <Grid />
-            <RecentProjects />
-            <Clients />
-            <Experience />
-            <Approach />
-            <Footer />
-          </div>
-          <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-            {/* Radial gradient for the container to give a faded look */}
-            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-          </div>
-          )
-        </main>
-      )}
+    <div id="some-element">
+      <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
+        (
+        <div className="max-w-7xl w-full flex flex-col">
+          <FloatingNav navItems={navItems} />
+          <Hero />
+          <Grid />
+          <RecentProjects />
+          <Clients />
+          <Experience />
+          <Approach />
+          <Footer />
+        </div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+        )
+      </main>
     </div>
   );
 }
