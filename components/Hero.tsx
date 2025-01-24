9f772d9a7adb0af2c9e5c4259fcb3454c3c6faea
Osuolale <mohd@yahoo.com>
2025-01-24 18:16:16 +0100

    Completing my portfolio...

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,16 +25,16 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         <div className="flex justify-center my-20 z-10">
           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-              Dynamic Magic with Next.js
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-90">
+              Dynamic Magic with Next.js and Android Studio
             </h2>
             <TextGenerateEffect
               className="text-center text-[40px] md:text-5xl lg:text-6xl"
               words="Transform Concepts into Seamless Digital Experiences"
             />
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-              Hi I&apos;m Osuolale, a Next.js and Android Kotlin Developer based
-              in Kaduna, Nigeria
+              Hi I&apos;m Ibrahim Osuolale Muhammed, a Next.js and Android
+              Kotlin Developer based in Kaduna, Nigeria
             </p>
             <a href="#about">
               <MagicButton
