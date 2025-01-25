991c46121bccbd9cbc41674f2aa2937d33a51c64
Osuolale <mohd@yahoo.com>
2025-01-25 20:16:41 +0100

    Fixing Bugs 8

--- components/ui/Grid.tsx
+++ components/ui/Grid.tsx
@@ -15,32 +15,34 @@ const BentoGridItem = dynamic(
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid>
-        {gridItems.map(
-          ({
-            id,
-            title,
-            description,
-            className,
-            img,
-            imgClassName,
-            titleClassName,
-            spareImg,
-          }) => (
-            <BentoGridItem
-              id={id}
-              key={id}
-              title={title}
-              description={description}
-              className={className}
-              img={img}
-              imgClassName={imgClassName}
-              titleClassName={titleClassName}
-              spareImg={spareImg}
-            />
-          )
-        )}
-      </BentoGrid>
+      <div>
+        <BentoGrid>
+          {gridItems.map(
+            ({
+              id,
+              title,
+              description,
+              className,
+              img,
+              imgClassName,
+              titleClassName,
+              spareImg,
+            }) => (
+              <BentoGridItem
+                id={id}
+                key={id}
+                title={title}
+                description={description}
+                className={className}
+                img={img}
+                imgClassName={imgClassName}
+                titleClassName={titleClassName}
+                spareImg={spareImg}
+              />
+            )
+          )}
+        </BentoGrid>
+      </div>
     </section>
   );
 };
