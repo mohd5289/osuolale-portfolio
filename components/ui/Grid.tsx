da99232b1452bac837fdc6a19397908438a16f49
Osuolale <mohd@yahoo.com>
2025-01-25 20:59:44 +0100

    Fixing Bugs 8

--- components/ui/Grid.tsx
+++ components/ui/Grid.tsx
@@ -1,49 +1,44 @@
-import React, { Suspense } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
 import { gridItems } from "@/data";
+import { BentoGridItem } from "./BentoGrid";
 
 // Dynamically import BentoGrid and BentoGridItem
 const BentoGrid = dynamic(
   () => import("./BentoGrid").then((mod) => mod.BentoGrid),
   { ssr: false }
 );
-const BentoGridItem = dynamic(
-  () => import("./BentoGrid").then((mod) => mod.BentoGridItem),
-  { ssr: false }
-);
 
 const Grid = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <section id="about">
-        <BentoGrid>
-          {gridItems.map(
-            ({
-              id,
-              title,
-              description,
-              className,
-              img,
-              imgClassName,
-              titleClassName,
-              spareImg,
-            }) => (
-              <BentoGridItem
-                id={id}
-                key={id}
-                title={title}
-                description={description}
-                className={className}
-                img={img}
-                imgClassName={imgClassName}
-                titleClassName={titleClassName}
-                spareImg={spareImg}
-              />
-            )
-          )}
-        </BentoGrid>
-      </section>
-    </Suspense>
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
   );
 };
 
