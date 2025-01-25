35c6683ecd0b9a51e4aa0dad499ccab8ffc3d167
Osuolale <mohd@yahoo.com>
2025-01-25 09:25:14 +0100

    Fixing Bugs 7

--- app/page.tsx
+++ app/page.tsx
@@ -9,28 +9,25 @@ import Grid from "@/components/ui/Grid";
 import { navItems } from "@/data";
 
 export default function Home() {
+  // Uncomment and use `isMounted` if you want to control client-side rendering
   // const [isMounted, setIsMounted] = useState(false);
 
   return (
-    <div id="some-element">
-      <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
-        (
-        <div className="max-w-7xl w-full flex flex-col">
-          <FloatingNav navItems={navItems} />
-          <Hero />
-          <Grid />
-          <RecentProjects />
-          <Clients />
-          <Experience />
-          <Approach />
-          <Footer />
-        </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-          {/* Radial gradient for the container to give a faded look */}
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-        </div>
-        )
-      </main>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full flex flex-col">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+    </main>
   );
 }
