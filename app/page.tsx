658bc4bc56f5841719427e4e4bdcee8111e8d6ec
Osuolale <mohd@yahoo.com>
2025-01-25 11:16:01 +0100

    Fixing Bugs 7

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
+// import Approach from "@/components/Approach";
+// import Clients from "@/components/Clients";
+// import Experience from "@/components/Experience";
+// import Footer from "@/components/Footer";
+// import Hero from "@/components/Hero";
+// import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Grid from "@/components/ui/Grid";
+// import Grid from "@/components/ui/Grid";
 import { navItems } from "@/data";
 
 export default function Home() {
@@ -16,13 +16,13 @@ export default function Home() {
       (
       <div className="max-w-7xl w-full flex flex-col">
         <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
-        <RecentProjects />
-        <Clients />
-        <Experience />
-        <Approach />
-        <Footer />
+        {/* <Hero /> */}
+        {/* <Grid /> */}
+        {/* <RecentProjects /> */}
+        {/* <Clients /> */}
+        {/* <Experience /> */}
+        {/* <Approach /> */}
+        {/* <Footer /> */}
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
