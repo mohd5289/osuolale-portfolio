e468302184e49fa96fa228199e9a25885c592cc3
Osuolale <mohd@yahoo.com>
2025-01-26 08:43:06 +0100

    Fixing Bugs 8

--- components/ui/Grid.tsx
+++ components/ui/Grid.tsx
@@ -7,38 +7,34 @@ import { BentoGrid, BentoGridItem } from "./BentoGrid";
 
 const Grid = () => {
   return (
-    <div>
-      <section id="about">
-        <BentoGrid>
-          <div>
-            {gridItems.map(
-              ({
-                id,
-                title,
-                description,
-                className,
-                img,
-                imgClassName,
-                titleClassName,
-                spareImg,
-              }) => (
-                <BentoGridItem
-                  id={id}
-                  key={id}
-                  title={title}
-                  description={description}
-                  className={className}
-                  img={img}
-                  imgClassName={imgClassName}
-                  titleClassName={titleClassName}
-                  spareImg={spareImg}
-                />
-              )
-            )}
-          </div>
-        </BentoGrid>
-      </section>
-    </div>
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
   );
 };
 
